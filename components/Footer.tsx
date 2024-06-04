8a8bfe7b2a208875f3f515b457a5cde544a8d805
arhaanb <arhaanb@gmail.com>
2024-06-04 23:31:57 +0530

    services page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,177 +1,169 @@
 import {
-    Box,
-    Button,
-    Flex,
-    Text,
-    useColorMode,
-    useColorModeValue,
+  Box,
+  Button,
+  Flex,
+  Text,
+  useColorMode,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
 import { Facebook, Linkedin, Twitter } from "react-feather";
 
 const Footer = () => {
-    const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
 
-    const socials = [
-        {
-            href: "https://www.facebook.com/MediaMorphosis1/",
-            icon: <Facebook width="inherit" />,
-        },
-        {
-            href: "https://twitter.com/media_morphosis",
-            icon: <Twitter width="inherit" />,
-        },
-        {
-            href: "https://www.linkedin.com/company/mediamorphosis-inc/",
-            icon: <Linkedin width="inherit" />,
-        },
-    ];
+  const socials = [
+    {
+      href: "https://www.facebook.com/MediaMorphosis1/",
+      icon: <Facebook width="inherit" />,
+    },
+    {
+      href: "https://twitter.com/media_morphosis",
+      icon: <Twitter width="inherit" />,
+    },
+    {
+      href: "https://www.linkedin.com/company/mediamorphosis-inc/",
+      icon: <Linkedin width="inherit" />,
+    },
+  ];
 
-    const linkBox1 = [
-        {
-            name: "Work",
-            href: "/work",
-        },
-        {
-            name: "Services",
-            href: "/services",
-        },
-        {
-            name: "Contact",
-            href: "/contact",
-        },
-    ];
+  const linkBox1 = [
+    {
+      name: "Work",
+      href: "/work",
+    },
+    {
+      name: "Services",
+      href: "/services",
+    },
+    {
+      name: "Contact",
+      href: "/contact",
+    },
+  ];
 
-    const linkBox2 = [
-        {
-            name: "About",
-            href: "https://mediamorphosisinc.com/about-us/",
-        },
-        {
-            name: "Ethnic Markets",
-            href: "https://mediamorphosisinc.com/ethnic-markets/",
-        },
-        {
-            name: "Ethnic Media",
-            href: "https://mediamorphosisinc.com/ethnic-media/",
-        },
-    ];
-    const linkBox3 = [
-        {
-            name: "Careers",
-            href: "https://mediamorphosisinc.com/careers/",
-        },
-        {
-            name: "Blog",
-            href: "https://mediamorphosisinc.com/blog/",
-        },
-    ];
+  const linkBox2 = [
+    {
+      name: "About",
+      href: "https://mediamorphosisinc.com/about-us/",
+    },
+    {
+      name: "Ethnic Markets",
+      href: "https://mediamorphosisinc.com/ethnic-markets/",
+    },
+    {
+      name: "Ethnic Media",
+      href: "https://mediamorphosisinc.com/ethnic-media/",
+    },
+  ];
+  const linkBox3 = [
+    {
+      name: "Careers",
+      href: "https://mediamorphosisinc.com/careers/",
+    },
+    {
+      name: "Blog",
+      href: "https://mediamorphosisinc.com/blog/",
+    },
+  ];
 
-    return (
-        <Flex
-            py={14}
-            className="container"
-            backgroundImage={`/assets/footer${useColorModeValue(
-                "light",
-                ""
-            )}.png`}
-            backgroundRepeat="none"
-            backgroundSize="cover"
-            h={{ base: "400px", "2xl": "500px" }}
-        >
-            <Box className="footer-social" w="50%" position="relative">
-                <Image
-                    src={`/assets/logo.svg`}
-                    alt="logo"
-                    width={200}
-                    height={150}
-                    style={{ marginBottom: "3rem" }}
-                />
-                <Flex>
-                    {socials.map((social, i) => (
-                        <Link
-                            key={i}
-                            href={social.href}
-                            target="_blank"
-                            rel="noopener"
-                        >
-                            <Button
-                                mx={{ base: "3px", lg: "6px", "2xl": 2 }}
-                                borderRadius="50%"
-                                border="1px solid"
-                                borderColor={
-                                    colorMode === "light" ? "black" : "white"
-                                }
-                                _hover={{ bg: "white", color: "black" }}
-                                minW={{
-                                    base: "40px",
-                                    lg: "50px",
-                                    "2xl": "55px",
-                                }}
-                                h={{ base: "40px", lg: "50px", "2xl": "55px" }}
-                                padding={{ base: "0 !important" }}
-                            >
-                                <Text
-                                    w={{
-                                        base: "1.2rem",
-                                        lg: "1.4rem",
-                                        "2xl": "1.6rem",
-                                    }}
-                                >
-                                    {social.icon}
-                                </Text>
-                            </Button>
-                        </Link>
-                    ))}
-                </Flex>
-
+  return (
+    <Flex
+      py={14}
+      className="container"
+      backgroundImage={`/assets/footer${useColorModeValue(
+          "light",
+          ""
+      )}.png`}
+    //   backgroundImage={"/assets/footer-transparent.png"}
+      backgroundRepeat="none"
+      backgroundSize="cover"
+      h={{ base: "400px", "2xl": "500px" }}
+    >
+      <Box className="footer-social" w="50%" position="relative">
+        <Image
+          src={`/assets/logo.svg`}
+          alt="logo"
+          width={200}
+          height={150}
+          style={{ marginBottom: "3rem" }}
+        />
+        <Flex>
+          {socials.map((social, i) => (
+            <Link key={i} href={social.href} target="_blank" rel="noopener">
+              <Button
+                mx={{ base: "3px", lg: "6px", "2xl": 2 }}
+                borderRadius="50%"
+                border="1px solid"
+                borderColor={colorMode === "light" ? "black" : "white"}
+                _hover={{ bg: "white", color: "black" }}
+                minW={{
+                  base: "40px",
+                  lg: "50px",
+                  "2xl": "55px",
+                }}
+                h={{ base: "40px", lg: "50px", "2xl": "55px" }}
+                padding={{ base: "0 !important" }}
+              >
                 <Text
-                    position="absolute"
-                    bottom={0}
-                    left={0}
-                    fontSize={{ base: 16, lg: 18, "2xl": 20 }}
-                    w={{ base: "200%", lg: "100%" }}
-                    textAlign={{ base: "center", lg: "left" }}
+                  w={{
+                    base: "1.2rem",
+                    lg: "1.4rem",
+                    "2xl": "1.6rem",
+                  }}
                 >
-                    <i>Copyright © Media Morphosis 2024 All Rights Reserved.</i>
+                  {social.icon}
                 </Text>
-            </Box>
-
-            <Flex
-                w="50%"
-                pt={{ base: 0, lg: 10, "2xl": 20 }}
-                h="100%"
-                direction={{ base: "column", lg: "row" }}
-                alignItems="flex-start"
-                pl={{ base: 14, lg: 0 }}
-                justifyContent={{ base: "flex-start", lg: "space-around" }}
-            >
-                {[1, 2, 3].map((j) => (
-                    <Box key={j}>
-                        {(j == 1 ? linkBox1 : j == 2 ? linkBox2 : linkBox3).map(
-                            (link) => (
-                                <Link href={link.href} key={link.name}>
-                                    <Text
-                                        _hover={{ color: "red" }}
-                                        transition="all 0.2s"
-                                        fontSize={{
-                                            base: 16,
-                                            lg: 18,
-                                            "2xl": 22,
-                                        }}
-                                        lineHeight={1.6}
-                                    >
-                                        {link.name}
-                                    </Text>
-                                </Link>
-                            )
-                        )}
-                    </Box>
-                ))}
-            </Flex>
+              </Button>
+            </Link>
+          ))}
         </Flex>
-    );
+
+        <Text
+          position="absolute"
+          bottom={0}
+          left={0}
+          fontSize={{ base: 16, lg: 18, "2xl": 20 }}
+          w={{ base: "200%", lg: "100%" }}
+          textAlign={{ base: "center", lg: "left" }}
+        >
+          <i>Copyright © Media Morphosis 2024 All Rights Reserved.</i>
+        </Text>
+      </Box>
+
+      <Flex
+        w="50%"
+        pt={{ base: 0, lg: 10, "2xl": 20 }}
+        h="100%"
+        direction={{ base: "column", lg: "row" }}
+        alignItems="flex-start"
+        pl={{ base: 14, lg: 0 }}
+        justifyContent={{ base: "flex-start", lg: "space-around" }}
+      >
+        {[1, 2, 3].map((j) => (
+          <Box key={j}>
+            {(j == 1 ? linkBox1 : j == 2 ? linkBox2 : linkBox3).map((link) => (
+              <Link href={link.href} key={link.name}>
+                <Text
+                  _hover={{ color: "red" }}
+                  transition="all 0.2s"
+                  fontSize={{
+                    base: 16,
+                    lg: 18,
+                    "2xl": 22,
+                  }}
+                  lineHeight={1.6}
+                >
+                  {link.name}
+                </Text>
+              </Link>
+            ))}
+          </Box>
+        ))}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Footer;
---
+++ pages/services/template.tsx
@@ -0,0 +1,201 @@
+import Consultation from "@/components/Consultation";
+import Italic from "@/components/Italic";
+import SlimHeading from "@/components/SlimHeading";
+import { cards } from "@/content";
+import {
+  As,
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useColorMode,
+  useDisclosure,
+  Image,
+  Divider,
+} from "@chakra-ui/react";
+import Head from "next/head";
+
+const Services = () => {
+  return (
+    <>
+      <Head>
+        <title>MediaMorphosis - Services</title>
+      </Head>
+
+      {/* <Box mt={{ base: 10, lg: 16, "2xl": 20 }}>
+        <SlimHeading w="100%" textAlign="center">
+          Digital Solutions Tailored for&nbsp;
+          <Italic>Impact</Italic>
+        </SlimHeading>
+      </Box> */}
+
+      <Box
+        py={"10em"}
+        px={{ base: 8, lg: 16, "2xl": 24 }}
+        backgroundImage={"/assets/banner.png"}
+        bgRepeat={"no-repeat"}
+        backgroundPosition={"center"}
+        backgroundSize={"cover"}
+      >
+        <Box w={["100%", "100%", "60%"]}>
+          {/* <Image objectFit={"cover"} src="/assets/banner.png" alt="banner" /> */}
+          <Text color={"white"} fontWeight={500} fontSize={80}>
+            Web Development
+          </Text>
+          <Text
+            color={"white"}
+            fontFamily={"PP Neue Montreal Book"}
+            fontSize={40}
+          >
+            Craft an App That Puts Your Business in Everyone&apos;s Pocket.
+          </Text>
+        </Box>
+      </Box>
+
+      <Box mt={{ base: 10, lg: 16, "2xl": 20 }}>
+        <Grid
+          px={{ base: 8, lg: 16, "2xl": 24 }}
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            sm: "repeat(2, 1fr)",
+          }}
+          mx={"25px"}
+          gap={[10, 10, 50]}
+          mt={{ base: 10, lg: 16, "2xl": 20 }}
+          gridAutoRows="auto"
+          pb={{ base: 10, lg: 4, "2xl": 0 }}
+          mb={10}
+        >
+          {/* {cards.map((card) => (
+            <ServiceCard key={card[0]} card={card} />
+          ))} */}
+          <GridItem w={["100%", "100%", "75%"]}>
+            <TextCard />
+          </GridItem>
+          <GridItem w={["100%", "100%", "75%"]}>
+            <Image src="/assets/services/phone.png" alt="Phone" />
+          </GridItem>{" "}
+          <GridItem w={["100%", "100%", "75%"]}>
+            <TextCard />
+          </GridItem>{" "}
+          <GridItem w={["100%", "100%", "75%"]}>
+            <TextCard />
+          </GridItem>
+        </Grid>
+      </Box>
+
+      <Consultation />
+
+      <Box mt={{ base: 10, lg: 10, "2xl": 10 }}>
+        <Divider mx={{ base: 23, lg: 31, "2xl": 39 }} />
+        <br />
+        <br />
+        <SlimHeading w="100%" textAlign="center">
+          Other Solutions
+        </SlimHeading>
+
+        <Grid
+          px={{ base: 8, lg: 16, "2xl": 24 }}
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            sm: "repeat(2, 1fr)",
+          }}
+          mx={"25px"}
+          gap={[20]}
+          mt={{ base: 10, lg: 16, "2xl": 20 }}
+          gridAutoRows="auto"
+          pb={{ base: 10, lg: 4, "2xl": 0 }}
+          mb={10}
+        >
+          {/* {cards.map((card) => (
+            <ServiceCard key={card[0]} card={card} />
+          ))} */}
+          <GridItem>
+            <OtherSolution />
+          </GridItem>
+          <GridItem>
+            <OtherSolution />
+          </GridItem>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+const TextCard = () => {
+  return (
+    <Box position={"relative"}>
+      <Text
+        position={"absolute"}
+        left={-7}
+        fontSize={150}
+        fontFamily="PP Neue Editorial"
+        fontStyle={"italic"}
+        color={"#FB3B3B33"}
+        top={"-80px"}
+      >
+        01
+      </Text>
+      <Box marginTop={100} marginLeft={[5, 5, 20]}>
+        <Text mb={2.5} fontWeight={500} fontSize={"30px"}>
+          FlowNote: SaaS tool for Web Design
+        </Text>
+        <Text>
+          Our app development service turns your ideas into intuitive,
+          user-friendly mobile applications that stand out in crowded
+          marketplaces. Whether it&apos;s for iOS, Android, or cross-platform
+          solutions, we focus on creating apps that offer engaging user
+          experiences, seamless functionality, and real-time performance
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
+const OtherSolution = () => {
+  return (
+    <Box minH={"25em"} position={"relative"}>
+      <Box
+        padding={"1.25em"}
+        background={"#E7EAF2"}
+        rounded={"xl"}
+        aspectRatio={1.5}
+        w={"70%"}
+      >
+        <Flex justifyContent={"space-between"} align="center">
+          <Text lineHeight={"40px"} fontSize="34px" fontWeight={415} flex="1">
+            Web Development
+          </Text>
+          <Flex justify="flex-end" flex="1">
+            <Image w="3em" src="/assets/services/webd.png" alt="Icon" />
+          </Flex>
+        </Flex>
+        <Text mt={3} fontWeight={400} fontSize={20}>
+          Bring Your Online Space to Life with a Stunning Website
+        </Text>
+      </Box>
+      <Flex
+        justify={"flex-end"}
+        ml={"-2em"}
+        marginTop={["-1em", "-2em", "-4em"]}
+      >
+        <Image
+          w="70%"
+          src="/assets/services/webd-banner.png"
+          alt="webd banner"
+        />
+      </Flex>
+    </Box>
+  );
+};
+
+export default Services;
